172e10b4c8357cb27dc1d2e63f29cffe339721d4
Binlong Li <herbert19lee@gmail.com>
2015-12-03 23:42:49 -0800

    finish the Chapter Quesiton 2

--- Chapter1/q2.cpp
+++ Chapter1/q2.cpp
@@ -3,5 +3,41 @@
 
 void reverse(char* s)
 {
+    if (s == nullptr)
+        return;
+    int len = strlen(s);
+    char temp;
+    // swap the characters in the string
+    for (int i = 0; i < len / 2; i++) {
+        temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+    }
+}
 
+//void reverse(char* s)
+//{
+//    if (s == nullptr)
+//        return;
+//
+//    char* end = s;
+//    while (*++end) {
+//    }
+//    --end;
+//
+//    char temp;
+//    while (s < end) {
+//        temp = *s;
+//        *s++ = *end;
+//        *end-- = temp;
+//    }
+//}
+
+int main()
+{
+    char s[] = "hello world";
+    printf("Org String is: %s\n", s);
+    reverse(s);
+    printf("New String is: %s\n", s);
+    return 0;
 }
