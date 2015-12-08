c189a57d4bfa2ecb11cfd954dcc1b73c8dff179e
Binlong Li <herbert19lee@gmail.com>
2015-12-08 14:27:35 -0800

    change the array interface from arr[][5] into int** arr, which is better suitable for dynamic allocation scenario.

--- Chapter1/C1Q6.cpp
+++ Chapter1/C1Q6.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+//#include "..\utils.h"
 
 typedef unsigned int uint;
 
@@ -18,7 +19,18 @@ void printMat(int arr[][5])
     }
 }
 
-void rotateMat(int arr[][5], uint s)
+template<class T>
+void print2DMat(T** mat, uint rows, uint cols)
+{
+    for (uint i = 0; i < rows; i++) {
+        for (uint j = 0; j < cols; j++) {
+            printf("%4d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void rotateMat(int** arr, uint s)
 {
     int t;
     for (uint loop = 0; loop < s / 2; loop++) {
@@ -33,19 +45,36 @@ void rotateMat(int arr[][5], uint s)
         }
 
         printf("\n This rotated matrix after loop %d is: \n", loop);
-        printMat(arr);
+        print2DMat<int>(arr, s, s);
 
     }
 
     printf("\n This rotated matrix is: \n");
-    printMat(arr);
+    print2DMat<int>(arr, s, s);
     
 }
+
+
 
 int main()
 {
     int arr[5][5] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
-    printMat(arr);
-    rotateMat(arr, 5);
+    // Dynamically create a 2D array
+    int** arrMat = new int*[5];
+    for (int i = 0; i < 5; i++) {
+        arrMat[i] = new int[5];
+    }
+
+    int count = 10; 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            arrMat[i][j] = ++count;
+        }
+    }
+
+    printf("dynamic allocation matrix is: \n");
+    print2DMat<int>(arrMat, 5, 5);
+
+    rotateMat(arrMat, 5);
     return 0;
 }
---
+++ utils.h
@@ -0,0 +1,17 @@
+// Utility functions for printing and generating data
+// Author: Binlong Li   12/08/2015
+
+#include <iostream>
+
+typedef unsigned int uint;
+
+template<T>
+void print2DMat(T** mat, uint rows, uint cols)
+{
+    for (uint i = 0; i < rows; i++) {
+        for (uint j = 0; j < cols; j++) {
+            printf("%4d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
