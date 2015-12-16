451b13ad9257d57ad60ea1430c4c27387c7c898c
Binlong Li <herbert19lee@gmail.com>
2015-12-16 09:48:19 -0800

    finish Chapter 2 Question 4.

--- Chapter2/C2Q4.cpp
+++ Chapter2/C2Q4.cpp
@@ -11,28 +11,43 @@
 #define SIZE 20
 using namespace std;
 
-void partitionLinkedList(Node<int>* head, int val)
+void partitionLinkedList(Node<int>** head, int val)
 {
-    Node<int> *anchor = head;
+    Node<int> *anchor = *head;
     Node<int> *anchorPrev = nullptr;
-    Node<int> *runner = head;
+    Node<int> *runner;
 
-    while (runner != nullptr) {
+    // anchor will be the first element larger than or equal to val (threshold)
+    while (anchor != nullptr) {
+        if (anchor->data < val) {
+            // anchorprev will be the last element less than val (threshold)
+            anchorPrev = anchor;
+            anchor = anchor->next;
+        } else {
+            break;
+        }
+
+    }
+    
+    // runner will start to run from the first element larger than val (threshold)
+    runner = anchor;
+    while (runner->next != nullptr) {
         
-        if (runner->data < val) {
-            // move the value to anchor
-            if (anchorPrev == nullptr) {
-                // The first item is less than val
-                anchorPrev = anchor;
-                anchor = anchor->next;
-                runner = runner->next;
+        if (runner->next->data < val) {            
+            // if the anchor is the first element, head of the whole linked list will be changed
+            // insert the first < val (threshold) value as head
+            if (anchorPrev == nullptr) {  
+                *head = runner->next;
+                anchorPrev = runner->next;
+                runner->next = runner->next->next;
+                anchorPrev->next = anchor;
             } else {                
-                anchorPrev->next = runner;
-                runner = runner->next;
+                // else just simply plug-in other element less than val (threshold)
+                anchorPrev->next = runner->next;
+                runner->next = runner->next->next;
                 anchorPrev->next->next = anchor;
-                anchorPrev = anchorPrev->next;
-            }
-            // anchor should always point to the first value larger than val
+                anchorPrev = anchorPrev->next;                
+            }            
         } else {
             // runner move to the next node
             runner = runner->next;
@@ -52,7 +67,7 @@ int main()
     head->printList();
 
     int value = 5;
-    partitionLinkedList(head, value);
+    partitionLinkedList(&head, value);
 
     printf("after partition: \n");
     head->printList();
