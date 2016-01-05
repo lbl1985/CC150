fef411a3a63e25996663d562c64525dab2cb8ae7
Binlong Li <bil@microsoft.com>
2016-01-05 09:55:56 -0800

    finish C3Q3

--- Chapter3/C3Q3.cpp
+++ Chapter3/C3Q3.cpp
@@ -12,19 +12,18 @@
 #include <stack>
 #include <vector>
 
-class SetOfStacks
-{
+const int isDebug = true;
+
+class SetOfStacks {
 public:
     void push(int v)
     {
         if (vStacks.empty()) {
-            stack<int> tmpStack; 
+            stack<int> tmpStack;
             tmpStack.push(v);
             vStacks.push_back(tmpStack);
-
-            vStacks.push_back(tmpStack);
         } else {
-            vector<stack<int>>::iterator itStack = vStacks.end();
+            vector<stack<int>>::iterator itStack = vStacks.end() - 1;
             if (itStack->size() < stackLimit) {
                 itStack->push(v);
             } else {
@@ -32,28 +31,91 @@ public:
                 tmpStack.push(v);
                 vStacks.push_back(tmpStack);
             }
-        }        
+        }
     }
 
     void pop()
     {
         if (!vStacks.empty()) {
-            vector<stack<int>>::iterator itStack = vStacks.end();
-            if (!itStack->empty()) {
-                itStack->pop();
-            } else {
+            vector<stack<int>>::iterator itStack = vStacks.end() - 1;
+            itStack->pop();
+            // after pop if the stack is empty, remove the stack from vector
+            if (itStack->empty()) {
                 vStacks.erase(itStack);
-                this->pop();
             }
         }
     }
+
+    int& top()
+    {
+        if (!vStacks.empty()) {
+            vector<stack<int>>::iterator itStack = vStacks.end() - 1;
+            return itStack->top();
+        }
+    }
+
+    void popat(int stackIdx)
+    {
+        if (!vStacks.empty()) {
+            if (vStacks.size() > (std::size_t)stackIdx) {
+                vector<stack<int>>::iterator itStack = vStacks.begin() + stackIdx;
+                itStack->pop();
+                if (itStack->empty()) {
+                    vStacks.erase(itStack);
+                }
+            }
+        }
+    }
+
+    int& topat(int stackIdx)
+    {
+        if (!vStacks.empty()) {
+            if (vStacks.size() > (std::size_t)stackIdx) {
+                vector<stack<int>>::iterator itStack = vStacks.begin() + stackIdx;
+                return itStack->top();
+            }
+        }
+    }
+
+    std::size_t size()
+    {
+        return vStacks.size();
+    }
+
+    bool empty()
+    {
+        return vStacks.empty();
+    }
+
 private:
-    static const int stackLimit = 30;
+    static const int stackLimit = 5;
     vector<stack<int>> vStacks;
 };
 
 int C3Q3()
 {
-    
+    SetOfStacks sos;
+    for (int i = 0; i < 30; i++) {
+        sos.push(i);
+        if (isDebug) {
+            printf("vStack size is: %d, vStack top is %d\n", sos.size(), sos.top());
+        }
+    }
+
+    printf("\n\n");
+    for (int i = 0; i < 5; i++) {
+        sos.popat(i);
+        if (isDebug) {
+            printf("vStack top at %d is %d\n", i, sos.topat(i));
+        }
+    }
+
+    printf("\n\n");
+    while (!sos.empty()) {
+        if (isDebug) {
+            printf("vStack top is %d\n", sos.top());
+        }
+        sos.pop();
+    }
     return 0;
 }
