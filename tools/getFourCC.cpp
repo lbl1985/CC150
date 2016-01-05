63b582bb6a103ff980c4d1a7e244c2d4e85336e0
Binlong Li <bil@microsoft.com>
2016-01-04 16:08:32 -0800

    add getFourcCC.cpp into tools.

---
+++ tools/getFourCC.cpp
@@ -0,0 +1,28 @@
+// Print out FOURCC
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+typedef unsigned int uint32_t;
+
+const bool isDebug = false;
+
+int main(int argc, char* argv[])
+{
+    uint32_t tmp = 0;
+    int n = strlen(argv[1]);
+    for (int i = 0; i < n; i++) {
+        if (isDebug) {
+            printf("%c", argv[1][i]);
+        }        
+        tmp += (argv[1][i] - '0') * (uint32_t)(pow(double(10), double(n - i- 1)));
+    }
+    if (isDebug) {
+        cout << endl << tmp << endl;
+    }    
+    
+    printf("%s is: %c%c%c%c", argv[1], tmp & 0xff, tmp>>8 & 0xff, tmp>>16 & 0xff, tmp>>24 & 0xff);
+
+    return 0;
+}
