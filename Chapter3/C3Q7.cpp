756e032fcb1028a7e233379f4cd80f12b488713c
Binlong Li <herbert19lee@gmail.com>
2016-01-05 23:32:15 -0800

    add question description for C3Q7

--- Chapter3/C3Q7.cpp
+++ Chapter3/C3Q7.cpp
@@ -1,3 +1,11 @@
+// 3.7 An animal shelter holds only dogs and cats, and operates on a strictly "first in, first
+// out" basis. People must dopt either the "oldest" (based on arrival time) of all animals
+// at the shelter, or they can select whether they would prefer a dog or a cat (and will
+// receive the oldest animal of that type). They canno select which specific animal they
+// would like. Create the data structures to maintain this system and implement ooperattions 
+// such as enqueue, dequeueAny, dequeueDog and dequeueCat. You may use the built-in 
+// LinkedList data structure.
+
 #include "..\inc\utils.h"
 
 int C3Q7()
