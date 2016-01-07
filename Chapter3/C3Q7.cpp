a811222f19ac93a82dcaa87a4b416d3f13b2c939
Binlong Li <herbert19lee@gmail.com>
2016-01-06 23:18:36 -0800

    C3Q7 first draft.

--- Chapter3/C3Q7.cpp
+++ Chapter3/C3Q7.cpp
@@ -7,6 +7,90 @@
 // LinkedList data structure.
 
 #include "..\inc\utils.h"
+#include <queue>
+using namespace std;
+
+enum AnimalType
+{
+    Dog = 0, 
+    Cat, 
+    Error
+};
+
+class Animal
+{
+public:
+    Animal(AnimalType iType, int iOrder) :Type(iType), order(iOrder) {}
+    int getOrder() { return order; }
+private: 
+    int order;
+    AnimalType Type;
+};
+
+class ManageQueue
+{    
+public:
+    ManageQueue() :order(0) {}
+
+    void enqueue(AnimalType at)
+    {
+        Animal tA(at, order++);
+        if (Dog == at) {            
+            DogQueue.push(tA);
+        } else if (Cat == at) {
+            CatQueue.push(tA);
+        }
+    }
+
+    Animal dequeueDog()
+    {
+        if (!DogQueue.empty()) {
+            Animal tA = DogQueue.front();
+            DogQueue.pop();
+            return tA;
+        } else {
+            printf("sorry dog queue is empty.\n");
+        }
+    }
+
+    Animal dequeueCat()
+    {
+        if (!CatQueue.empty()) {
+            Animal tA = CatQueue.front();
+            CatQueue.pop();
+            return tA;
+        } else {
+            printf("sorry cat queue is empty.\n");
+        }
+    }
+
+    Animal dequeueAny()
+    {
+        if (CatQueue.empty() && !DogQueue.empty()) {
+            return dequeueDog();
+        } else if (!CatQueue.empty() && DogQueue.empty()) {
+            return dequeueCat();
+        } else if (CatQueue.empty() && DogQueue.empty()) {
+            printf("All our queue are empty.\n");
+        } else {
+            Animal tC = CatQueue.front();   
+            Animal tD = DogQueue.front();
+            if (tC.getOrder() < tD.getOrder) {
+                Animal tA = tC;
+                dequeueCat();
+                return tA;
+            } else {
+                Animal tA = tD;
+                dequeueDog();
+                return tA;
+            }            
+        }
+    }
+private: 
+    queue<Animal>  DogQueue;
+    queue<Animal> CatQueue;
+    int order;
+};
 
 int C3Q7()
 {
