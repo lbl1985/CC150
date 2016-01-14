b02afd922a9f10023436d7fb08ae4dfb10ffb5bc
Binlong Li <herbert19lee@gmail.com>
2016-01-14 00:28:16 -0800

    finish one version with bubble sort, but the time limit exceeded. Need more thinking for this question.

--- leetCode/Q324_WiggleSortII.cpp
+++ leetCode/Q324_WiggleSortII.cpp
@@ -19,8 +19,55 @@
 #include "..\inc\utils.h"
 #include <vector>
 
+void bubbleSort(vector<int>& arr)
+{
+	int sz = (int)arr.size();
+	int temp;
+	for (int endIndex = sz - 1; endIndex > 0; endIndex--) {
+		for (int i = 0; i < endIndex; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+	}
+}
+
+void wiggleSort(vector<int>& num) {
+	bubbleSort(num);
+	size_t sz = num.size();
+	int halfSize = sz / 2;
+	int offset = sz % 2 == 0 ? halfSize : halfSize + 1;
+
+	vector<int> numSorted(num);
+	int index = 0;
+	for (int i = 0; i < halfSize; i++) {
+		num[index++] = numSorted[i];
+		num[index++] = numSorted[i + offset];
+	}
+	if (index < sz - 1) {
+		num[index++] = numSorted[halfSize];
+	}
+}
+
+void printVector(const vector<int> &v) {
+	for (size_t i = 0; i < v.size(); i++) {
+		printf("%d ", v[i]);
+	}
+}
+
 int Q324_WiggleSortII()
 {
-	cout << "Q324_WiggleSortII" << endl;
+	vector<vector<int> > tests{ { 1, 5, 1, 1, 6, 4 }, { 1, 3, 2, 2, 3, 1} };
+	for (size_t i = 0; i < tests.size(); i++) {
+		printf("test set %d: \n", i);
+		printVector(tests[i]);
+		wiggleSort(tests[i]);
+		printf("after wiggle sort: \n");
+		printVector(tests[i]);
+		printf("\n");
+	}
+	
 	return 0;
 }
