a53a1f21ec47dd9fa0d7bd7fbbf4924f5812c3ef
Binlong Li <herbert19lee@gmail.com>
2016-01-15 21:40:35 -0800

    quick sort test finish.

--- Chapter10_sort/quicksort.cpp
+++ Chapter10_sort/quicksort.cpp
@@ -2,8 +2,53 @@
 // Author: Binlong Li  Date: 01/15/2016
 
 #include "..\inc\utils.h"
+#include <vector>
+
+int partition(vector<int> &arr, size_t low, size_t high){
+	int p = arr[high];
+	size_t smallIndex = low;
+	int tmp = 0;
+	for(size_t i = low; i < high; i++){
+		if(arr[i] <= p){
+			tmp = arr[i];
+			arr[i] = arr[smallIndex]; 
+			arr[smallIndex] = tmp;
+			smallIndex++;
+		}		
+	}
+
+	tmp = arr[smallIndex];
+	arr[smallIndex] = arr[high];
+	arr[high] = tmp;
+	
+	return smallIndex;
+}
+
+void quicksort(vector<int> &arr, int low, int high){
+	if(low < high){
+		int middle = partition(arr, low, high);
+		quicksort(arr, low, middle - 1);
+		quicksort(arr, middle + 1, high);
+	}
+}
+
 
 int quicksortTest(){
 	cout << "This is quicksortTest" << endl;
+
+	vector<int> arr{ 0, -2, 3, 9, 8, 67, 88, 92, -30, 50 };
+	cout << "the array is: \n";
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	quicksort(arr, 0, arr.size()- 1);
+	cout << "After quick sort the array is: \n";
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
 	return 0;
 }
