127ff3de1102f2f6cd09c3775e9c20b858558d2a
Binlong Li <herbert19lee@gmail.com>
2016-01-30 22:51:08 -0800

    first draft of C4Q4

--- Chapter4_tree/C4Q4.cpp
+++ Chapter4_tree/C4Q4.cpp
@@ -5,8 +5,65 @@
 // Date:   01/29/2016
 
 #include "..\inc\utils.h"
+#include "..\inc\binarytree.h"
+#include <vector>
+#include <stack>
+using namespace std;
+
+void getList(BinaryTreeNode<int>* root, vector<vector<BinaryTreeNode<int>*> >& vNodes)
+{
+	if(nullptr == root){
+		return;
+	}
+	stack<BinaryTreeNode<int>*> s;
+	stack<int> sDepth;
+
+	s.push(root);
+	sDepth.push(0);
+	root->st = visiting;
+
+
+	while(!s.empty()){
+		BinaryTreeNode<int>* cur = s.top();
+		s.pop();
+		int curDepth = sDepth.top();
+		sDepth.pop();
+
+		if((int)vNodes.size() < curDepth + 1){
+			vector<BinaryTreeNode<int>*> tmpVec;
+			vNodes.push_back(tmpVec);
+		}
+		vector<BinaryTreeNode<int>*>& curVec = vNodes[curDepth];
+
+		if(cur->right != nullptr && cur->st != visited ){
+			s.push(cur->right);
+			sDepth.push(curDepth + 1);
+			cur->right->st = visiting;
+		}
+
+		if(cur->left != nullptr && cur->st != visited){
+			s.push(cur->left);
+			sDepth.push(curDepth + 1);
+			cur->left->st = visiting;
+		}
+
+		cur->st = visited;
+		curVec.push_back(cur);
+	}
+}
 
 int C4Q4(){
-	cout << "inside C4Q4" << endl;
+	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};	
+	BinaryTreeNode<int> *root = createMinimalBST<int>(arr, 0, 9);
+
+	vector<vector< BinaryTreeNode<int>* > >	 vNodes;
+	getList(root, vNodes);
+
+	for(int i = 0; i < (int)vNodes.size(); i++){
+		for(int j = 0; j < (int)vNodes[i].size(); j++){
+			printf("%d ", vNodes[i][j]->data);
+		}
+		printf("\n");
+	}
 	return 0;
 }
