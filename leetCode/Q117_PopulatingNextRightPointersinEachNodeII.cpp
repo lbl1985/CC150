4dc9506c4f3c71fd1110ac579d8ebb6dfbcba656
Binlong <bil@microsoft.com>
2016-02-28 17:40:56 -0800

    Q117

---
+++ leetCode/Q117_PopulatingNextRightPointersinEachNodeII.cpp
@@ -0,0 +1,33 @@
+// Follow up for problem "Populating Next Right Pointers in Each Node".
+// 
+// What if the given tree could be any binary tree? Would your previous solution still work?
+// 
+// Note: 
+// •You may only use constant extra space.
+// 
+// 
+// For example,
+//  Given the following binary tree,
+// 
+//          1
+//        /  \
+//       2    3
+//      / \    \
+//     4   5    7
+// 
+// 
+// 
+// After calling your function, the tree should look like:
+// 
+//          1 -> NULL
+//        /  \
+//       2 -> 3 -> NULL
+//      / \    \
+//     4-> 5 -> 7 -> NULL
+// 
+// Author: Binlong LI
+// Date: 02/28/2016
+// Finish: 02/28/2016
+// 
+// This question has identical soution as Q116_PopulatingNextRightPointersinEachNode
+// Even though the solution efficency is not great, but it does work.
