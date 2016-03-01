72ab55f3287864fdccca05387deb03bf104deac9
Binlong <bil@microsoft.com>
2016-02-29 22:19:14 -0800

    first try of Q051 N Queen problem, still not solved yet.

--- leetCode/Q051_N_Queens.cpp
+++ leetCode/Q051_N_Queens.cpp
@@ -29,7 +29,101 @@
 
 #include "..\inc\utils.h"
 
+#include <vector>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+struct QueenNode{
+	QueenNode(int v):val(v){}
+	int val;
+	vector<QueenNode*> next;
+};
+
+void removeItem(unordered_set<int>& neighbor, int val)
+{
+	unordered_set<int>::iterator fiter = neighbor.find(val);
+	if(fiter != neighbor.end()){
+		neighbor.erase(fiter);
+	}
+}
+
+vector<QueenNode*> getPossibleChild(const unordered_set<int>& fullSet, const vector<int>& curSeq)
+{
+	int n = (int)fullSet.size();
+	unordered_set<int> neighbor(fullSet);
+
+	for(size_t i = 0; i < curSeq.size(); i++){
+		removeItem(neighbor, curSeq[i]);
+	}
+
+	int curId = curSeq[curSeq.size() - 1];
+	if(curId == 0){
+		removeItem(neighbor, curId + 1);
+	} else if(curId == n - 1){
+		removeItem(neighbor, curId - 1);
+	} else {
+		removeItem(neighbor, curId - 1);
+		removeItem(neighbor, curId + 1);
+	}
+
+	vector<QueenNode*> neighborList;
+	if(!neighbor.empty()){
+		for(unordered_set<int>::iterator it = neighbor.begin(); it != neighbor.end(); it++){			
+			neighborList.push_back(new QueenNode(*it));
+		}
+	}
+
+	return neighborList;
+}
+
+void createNext(QueenNode* root, const unordered_set<int>& fullSet, const vector<int>& curSeq, vector<vector<int>>& idx){
+	size_t targetLength = fullSet.size();
+	root->next = getPossibleChild(fullSet, curSeq);
+	for(size_t i = 0; i < root->next.size(); i++){
+		vector<int> tmpSeq(curSeq);
+		tmpSeq.push_back(root->next[i]->val);
+
+		if((int)curSeq.size() == targetLength - 1){			
+			idx.push_back(tmpSeq);
+		} else{			
+			createNext(root->next[i], fullSet, tmpSeq, idx);
+		}
+	}
+}
+
+vector<vector<string>> solveNQueens(int n) 
+{
+	vector<vector<string>> res;
+	vector<vector<int>> idx;
+	unordered_set<int> fullSet;
+	for(int i = 0; i < n; i++){
+		fullSet.insert(i);
+	}
+	// for each number to be as root
+	for(int i = 0; i < n; i++){		
+		vector<int> curSeq;
+		curSeq.push_back(i);
+		QueenNode* root = new QueenNode(i);
+		createNext(root, fullSet, curSeq, idx);
+	}
+	printf("# of NQueen solution for %d is: %d\n", n, idx.size());
+	for(size_t i = 0; i< idx.size(); i++){
+		printf("%d:", i);
+		for(size_t j = 0; j< idx[i].size(); j++){
+			printf("%d ", idx[i][j]);
+		}
+		printf("\n");
+	}
+	return res;
+}
+
 int Q051_N_Queens(){
 	printf("inside of Q051_N_Queens();\n");
+	solveNQueens(4);
+	solveNQueens(5);
+	// solveNQueens(6);
+	// solveNQueens(7);
+	// solveNQueens(8);
 	return 0;
 }
