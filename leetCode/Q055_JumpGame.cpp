364f4fd5c7161f7defd9fceca5d0c1b638cb3b30
Binlong Li <herbert19lee@gmail.com>
2016-03-04 23:24:16 -0800

    add one more test case, which I failed previously.

--- leetCode/Q055_JumpGame.cpp
+++ leetCode/Q055_JumpGame.cpp
@@ -40,12 +40,15 @@ int Q055_JumpGame()
 	printf("Q055_JumpGame\n");
 	int ex1[] = {2, 3, 1, 1, 4};
 	int ex2[] = {3, 2, 1, 0, 4};
+	int ex3[] = {0, 2, 3};
 
 	vector<vector<int>> test;
 	vector<int> tmp1(ex1, ex1 + 5);
 	test.push_back(tmp1);
 	vector<int> tmp2(ex2, ex2 + 5);
 	test.push_back(tmp2);
+	vector<int> tmp3(ex3, ex3 + 3);
+	test.push_back(tmp3);
 
 	for(size_t i = 0; i < test.size(); i++){
 		printVector<int>(test[i]);
