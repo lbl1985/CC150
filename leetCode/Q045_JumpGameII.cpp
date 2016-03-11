e13a8cc7a62dd6543a8d990136ba31f1d5737494
Binlong <bil@microsoft.com>
2016-03-10 23:55:07 -0800

    solve Q045

--- leetCode/Q045_JumpGameII.cpp
+++ leetCode/Q045_JumpGameII.cpp
@@ -21,46 +21,44 @@
 // 
 // Author: Binlong Li
 // Date:  03/03/2016
+// Solved: 03/10/2016
 
 #include "..\inc\utils.h"
 
 #include <vector>
 using namespace std;
+#define DEBUG 1
 
 int jump(vector<int>& nums)
 {
 	size_t sz = nums.size();
 	if(sz <= 1){
 		return 0;
-	}
-	vector<int> idxQueue;
-
-	bool reach = false;		
+	}	
+	int step = 0;
 	int curIdx =0;	
 
-	while(!reach){
-		idxQueue.push_back(curIdx);
+	while(true){
+		step++;
 		int maxV = -1;
 		int maxIdx = -1;
-		for(int i = 0; i < nums[curIdx]; i++){
-			int curV = nums[curIdx + i + 1];
+		int curStep = nums[curIdx];
+		for(int i = curStep; i > 0; i--){
+			if(curIdx + i >= (int)(sz - 1)){
+				return step;
+			}
+			int curV = nums[curIdx + i] + curIdx + i;
+			if(curV >= (int)(sz-1)){
+				return ++step;
+			}
 			if(curV > maxV){
 				maxV = curV;
-				maxIdx = curIdx + i + 1;
+				maxIdx = curIdx + i;
 			}
 		}
-		if(maxIdx + maxV >= (int)(sz - 1)){
-			idxQueue.push_back(maxIdx);
-			reach = true;
-		} else {
-			curIdx = maxIdx;
-		}
+		curIdx = maxIdx;
 	}	
-	
-	cout << "Need " << idxQueue.size() << " steps: " << endl;
-	printVector<int>(idxQueue);
-	cout << endl;
-	return (int)idxQueue.size();
+	return step;
 }
 
 int Q045_JumpGameII()
@@ -68,14 +66,18 @@ int Q045_JumpGameII()
 	printf("inside of Q045_JumpGameII\n");
 	vector<int> exp1{2, 3, 1, 1, 4};
 	vector<int> exp2{0};
+	vector<int> exp3{1, 2};
 
 	cout << "test case 1: " << endl;
 	printVector<int>(exp1);
-	jump(exp1);
+	cout << "Need " << jump(exp1) << " steps" << endl << endl;
 
 	cout << "test case 2: " << endl;
 	printVector<int>(exp2);
-	jump(exp2);
+	cout << "Need " << jump(exp2) << " steps" << endl << endl;
 
+	cout << "test case 3:" << endl;
+	printVector<int>(exp3);
+	cout << "Need " << jump(exp3) << " steps" << endl << endl;
 	return 0;
 }
