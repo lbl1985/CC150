52568119bf123a83bfac7e17f365232ca41f006b
Binlong Li <herbert19lee@gmail.com>
2016-04-20 23:15:42 -0700

    test code for using std map container.

--- leetCode/Q149_Max_Points_on_a_line.cpp
+++ leetCode/Q149_Max_Points_on_a_line.cpp
@@ -20,9 +20,37 @@
 #include <map>
 #include <vector>
 
+
 using namespace std;
+
+struct line{
+	double k;
+	double b;
+	line():k(0), b(0) {}
+	line(double ki, double bi):k(ki), b(bi){}
+};
+
 int maxPoints(vector<Point>& points)
 {
+	map<line, vector<Point>> lines; // non-vertical line
+	map<int, vector<Point>> ver; // vertical line
+
+	int sz = points.size();
+	if(sz == 0 || sz == 1){
+		return 0;
+	}
+
+	for(int i = 1; i < sz; i++){
+		Point p1 = points[i];
+		for(int j = 0; j < i; j++){
+			Point p2 = points[j];
+			if(p1.x == p2.x){
+				if(ver.count(p1.x) > 0){
+
+				}
+			}
+		}
+	}
 	return 0;
 }
 
@@ -46,5 +74,19 @@ int Q149_Max_Points_on_aline()
 	// Test case 1
 	vector<Point> tc1 = createTestCase1();
 	printf("result of test case 1 is: %d\n", maxPoints(tc1));
+
+	map<int, vector<int>> testMap;
+	vector<int> tmpv;
+	tmpv.push_back(1);
+	tmpv.push_back(2);
+	testMap[1] = tmpv;
+	printVector<int>(testMap[1]);
+
+	map<int, vector<int>>::iterator mapIter = testMap.find(1);
+	mapIter->second.push_back(3);
+
+	// vector<int> tmpv3 = testMap[1];
+	printVector<int>(testMap[1]);
+
 	return 0;
 }
