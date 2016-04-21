5bbceef0acf639ddba491a9fd1d5324e7e60de89
Binlong <bil@microsoft.com>
2016-04-21 12:01:51 -0700

    construct char works.

--- exp/constructChar.cpp
+++ exp/constructChar.cpp
@@ -1,7 +1,19 @@
 #include "..\inc\utils.h"
 
+class charTest {
+public:
+	charTest():i(1), c("hello"){
+
+	}
+	int i;
+	char* c;
+};
+
 int constructChar()
 {
 	printf("insdie of constructChar\n");
+	charTest cClass;
+	printf("%d", cClass.i);
+	printf("%s", cClass.c);
 	return 0;
 }
