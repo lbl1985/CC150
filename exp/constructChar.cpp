f1ae38bf1589ba1d05db0d0e538e4b6368c232ab
Binlong <bil@microsoft.com>
2016-04-25 09:45:27 -0700

    test for unique pointer.

--- exp/constructChar.cpp
+++ exp/constructChar.cpp
@@ -1,4 +1,7 @@
 #include "..\inc\utils.h"
+#include <memory>
+
+using namespace std;
 
 class charTest {
 public:
@@ -9,11 +12,28 @@ public:
 	char* c;
 };
 
+static int ct = 0;
+
+class testArray {
+public:
+	testArray() {
+		i = ct++;
+		printf("create %d\n", i);
+	}
+	~testArray(){
+		printf("release item %d\n", i);
+	}
+private:
+	int i;
+};
+
 int constructChar()
 {
 	printf("insdie of constructChar\n");
 	charTest cClass;
 	printf("%d", cClass.i);
 	printf("%s", cClass.c);
+	const int sz = 5;
+	unique_ptr<testArray []> new_array(new testArray[sz]);
 	return 0;
 }
