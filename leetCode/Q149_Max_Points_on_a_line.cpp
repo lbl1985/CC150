fa59306b3a63a8060750fe1e7a5cc3e568f2c8f0
Binlong Li <herbert19lee@gmail.com>
2016-05-03 23:34:42 -0700

    finally finishe Q149.

--- leetCode/Q149_Max_Points_on_a_line.cpp
+++ leetCode/Q149_Max_Points_on_a_line.cpp
@@ -19,6 +19,7 @@
 // [[29,87],[145,227],[400,84],[800,179],[60,950],[560,122],[-6,5],[-87,-53],[-64,-118],[-204,-388],[720,160],[-232,-228],[-72,-135],[-102,-163],[-68,-88],[-116,-95],[-34,-13],[170,437],[40,103],[0,-38],[-10,-7],[-36,-114],[238,587],[-340,-140],[-7,2],[36,586],[60,950],[-42,-597],[-4,-6],[0,18],[36,586],[18,0],[-720,-182],[240,46],[5,-6],[261,367],[-203,-193],[240,46],[400,84],[72,114],[0,62],[-42,-597],[-170,-76],[-174,-158],[68,212],[-480,-125],[5,-6],[0,-38],[174,262],[34,137],[-232,-187],[-232,-228],[232,332],[-64,-118],[-240,-68],[272,662],[-40,-67],[203,158],[-203,-164],[272,662],[56,137],[4,-1],[-18,-233],[240,46],[-3,2],[640,141],[-480,-125],[-29,17],[-64,-118],[800,179],[-56,-101],[36,586],[-64,-118],[-87,-53],[-29,17],[320,65],[7,5],[40,103],[136,362],[-320,-87],[-5,5],[-340,-688],[-232,-228],[9,1],[-27,-95],[7,-5],[58,122],[48,120],[8,35],[-272,-538],[34,137],[-800,-201],[-68,-88],[29,87],[160,27],[72,171],[261,367],[-56,-101],[-9,-2],[0,52],[-6,-7],[170,437],[-261,-210],[-48,-84],[-63,-171],[-24,-33],[-68,-88],[-204,-388],[40,103],[34,137],[-204,-388],[-400,-106]]
 // Author: Binlong Li
 // Date: 04/19/2016
+// Finish: 05/03/2016
 #include "..\inc\utils.h"
 #include <map>
 #include <vector>
@@ -27,54 +28,8 @@
 
 using namespace std;
 
-struct line{
-	double k;
-	int b;
-	int count;
-	double key;
-	line():k(0), b(0) {}
-	line(double ki, int bi):k(ki), b(bi), count(1){ key = k * 1000 + b;}
-	bool operator == (const line &rhs) const { return this->k == rhs.k && this->b == rhs.b;}
-
-};
-
-// line management class
-class line_man{
-public:
-	void checkline(const line &il, int& max);
-private: 
-	// vector<line> lines;
-	map<double, int> lines;
-};
-
-void line_man::checkline(const line & il, int& max)
-{
-	// Go over all the lines in the vector
-	// vector<line>::iterator it = find(lines.begin(), lines.end(), il);
-	// if(it != lines.end()) {
-	// 	(*it).count++;
-	// 	max = (*it).count > max ? (*it).count : max;
-	// } else {
-	// 	lines.push_back(il);
-	// }
-	map<double, int>::iterator it = lines.find(il.key);
-	if(it != lines.end()) {
-		it->second++;
-		int count = it->second;
-		max = count > max ? count : max;
-	} else {
-		lines.insert(pair<double, int>(il.key, 2));
-		max = 2 > max ? 2 : max;
-	}
-
-}
-
 int maxPoints(vector<Point>& points)
 {
-	map<line, int> lines; // non-vertical line
-	map<int, int> ver; // vertical line
-
-	line_man lm;
 	int sz = points.size();
 
 	if(sz == 0){
@@ -85,30 +40,43 @@ int maxPoints(vector<Point>& points)
 
 	// The first point will be saved in ver
 	int max = 0;
+	map<double, int> lines; // first->slope, second->count
+	map<double, int>::iterator it;
+	int verPoints = 1;		
+	int identicalPoint = 0;
 	for(int i = 0; i < sz; i++){
-		Point p1 = points[i];
-		map<int, int>::iterator v_iter = ver.find(p1.x);
-		if(v_iter != ver.end()){
-			v_iter->second++;
-			if(v_iter->second > max) {
-				max = v_iter->second;
-			}
-		} else {
-			ver[p1.x] = 1;
-			if(1 > max) {
-				max = 1;
+		for(int j = 0; j < sz; j++) {
+			if(i != j) {
+				if(points[i].x == points[j].x) {
+					verPoints++;
+					max = verPoints > max ? verPoints : max;
+				} else {
+					double slope = (double)(points[j].y - points[i].y) / (double)(points[j].x - points[i].x);
+					it = lines.find(slope);
+					if(it != lines.end()) {
+						it->second++;
+						max = it->second > max ? it->second : max;
+					} else {
+						lines.insert(pair<double, int>(slope, 2));
+						max = max ? max : 2;
+					}
+				}
+
+                if (points[i].x == points[j].x && points[i].y == points[j].y) {
+                    identicalPoint++;
+                }
 			}
 		}
 
-
-
-		for(int j = 0; j < i; j++){
-			Point p2 = points[j];
-			if(p2.x != p1.x) {
-				line tmpLine(double(p2.y - p1.y) / double(p2.x - p1.x), p1.y - p2.y);
-				lm.checkline(tmpLine, max);
-			}
+		if(identicalPoint) {
+			for(it = lines.begin(); it != lines.end(); it++) {
+				it->second += identicalPoint;
+				max = it->second > max ? it->second : max;
+			}			
 		}
+		lines.clear();
+		identicalPoint = 0;
+		verPoints = 1;
 	}
 	return max;
 }
@@ -127,15 +95,42 @@ vector<Point> createTestCase1()
 	return tmpv;
 }
 
+vector<Point> createTestCase2()
+{
+	std::vector<Point> v;
+	v.push_back(Point(1, 1));
+	v.push_back(Point(1, 1));
+	v.push_back(Point(2, 2));
+	v.push_back(Point(2, 2));
+	return v;
+}
+
+vector<Point> createTestCase3()
+{
+	std::vector<Point> v;
+	v.push_back(Point(84, 250));
+	v.push_back(Point(0,0));
+	v.push_back(Point(1,0));
+	v.push_back(Point(0,-70));
+	v.push_back(Point(0,-70));
+	v.push_back(Point(1,-1));
+	v.push_back(Point(21,10));
+	v.push_back(Point(42,90));
+	v.push_back(Point(-42,-230));
+	return v;
+}
+
 int Q149_Max_Points_on_aline()
 {
 	printf("inside Q149_Max_Points_on_aline\n");
 	// Test case 1
 	vector<Point> tc1 = createTestCase1();
-	printf("result of test case 1 is: %d\n", maxPoints(tc1));
+	printf("result of test case 1, expect 5, result is: %d\n", maxPoints(tc1));
 
-	int max1 = maxPoints(tc1);
-	printf("test case 1 result: %d\n", max1);
+	std::vector<Point> tc2 = createTestCase2();
+	printf("result of test case 2, expect 4, result is: %d\n", maxPoints(tc2));
 
+	std::vector<Point> tc3 = createTestCase3();
+	printf("result of test case 3, expect 6, result is: %d\n", maxPoints(tc3));
 	return 0;
 }
