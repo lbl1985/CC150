7085abb18ae9d25b4e28b737bafa746bc258239c
Binlong Li <herbert19lee@gmail.com>
2016-05-18 09:54:58 -0700

    add Q349_IntersectionOfTwoArrays

---
+++ leetCode/Q349_IntersectionOfTwoArrays.cpp
@@ -0,0 +1,24 @@
+// Intersection of Two Arrays
+// Given two arrays, write a function to compute their intersection. 
+
+// Example:
+//  Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2]. 
+
+// Note:
+
+// •Each element in the result must be unique.
+// •The result can be in any order.
+
+// vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+// }
+// Author: Binlong Li
+// Date: 05/18/2016
+
+#include "..\inc\utils.h"
+
+int Q349_IntersectionOfTwoArrays()
+{
+	printf ("insdie of Q349_IntersectionOfTwoArrays\n");
+	return 0;
+}
