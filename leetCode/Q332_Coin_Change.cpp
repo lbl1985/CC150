639bf9ad8194538a12ca19e1fcb0c614d20211b9
Binlong Li <herbert19lee@gmail.com>
2016-05-24 22:36:41 -0700

    Q332 quick solution does not really works.

--- leetCode/Q332_Coin_Change.cpp
+++ leetCode/Q332_Coin_Change.cpp
@@ -23,8 +23,37 @@
 using namespace std;
 
 int coinChange(vector<int>& coins, int amount) {
-	int res = 0;
+	int res = -1;
+	int sz = (int)coins.size();
 	sort(coins.begin(), coins.end());
+	if(amount == 0) {
+		return 0;
+	}
+	// amount is less than the smallest of the coins, return -1
+	if(amount < coins[0]) {
+		return -1;
+	}
+	for(int i = 0; i < sz; i++) {
+		if(amount == coins[i]) {
+			return 1;
+		}
+	}
+	for(int i = sz - 1; i >= 0; i--) {		
+		if (amount < coins[i]) {
+			continue;
+		}
+		int n = amount / coins[i];
+		int tRes = coinChange(coins, amount - n * coins[i]);
+		if (tRes == -1 && sz > 1) {
+			n = n - 1;
+			tRes = coinChange(coins, amount - n * coins[i]);
+			if(tRes != -1) {
+				return tRes + n;
+			}
+		} else if(sz > 1) {
+			return tRes + n;
+		}
+	}
 	
 	return res;
 }
@@ -33,7 +62,34 @@ int coinChange(vector<int>& coins, int amount) {
 int Q332_Coin_Change()
 {
 	printf("inside of Q332_Coin_Change\n");
+	cout << "test 1" << endl;
 	vector<int> test1{5, 1, 2};
-	coinChange(test1, 11);
+	int res1 = coinChange(test1, 11);
+	printVector(test1); cout << ", 11" << endl;
+	cout << "expect: 3" << endl;
+	cout << "result: " << res1 << endl;
+
+	cout << "test 2" << endl;
+	vector<int> test2{2};
+	int res2 = coinChange(test2, 3);
+	printVector(test2); cout << ", 3" << endl;
+	cout << "expect: -1" << endl;
+	cout << "result: " << res2 << endl;
+
+	cout << "test 3" << endl;
+	vector<int> test3{1, 3};
+	int res3 = coinChange(test3, 2);
+	printVector(test3); cout << ", 2" << endl;
+	cout << "expect: 2" << endl;
+	cout << "result: " << res3 << endl;
+
+	cout << "test4" << endl;
+	vector<int> test4{186,419,83,408};
+	int res4 = coinChange(test4, 2);
+	printVector(test4); cout << ", 6249" << endl;
+	cout << "expect: None" << endl;
+	cout << "result: " << res4 << endl;
+
+
 	return 0;
 }
--- tools/main.cpp
+++ tools/main.cpp
@@ -106,10 +106,10 @@ int main()
     // res = Q334_Increasing_Triplet_Subsequence();
     // cout << endl << "Q332_Reconstruct_Itinerary" << endl;
     // res = Q332_Reconstruct_Itinerary();
-    // cout << endl << "Q332_Coin_Change" << endl;
-    // res = Q332_Coin_Change();
-    cout << endl << "Q189_Rotate_Array" << endl;
-    res = Q189_Rotate_Array();
+    cout << endl << "Q332_Coin_Change" << endl;
+    res = Q332_Coin_Change();
+    // cout << endl << "Q189_Rotate_Array" << endl;
+    // res = Q189_Rotate_Array();
 
     // Exp
     // cout << endl << "constructChar" << endl;
