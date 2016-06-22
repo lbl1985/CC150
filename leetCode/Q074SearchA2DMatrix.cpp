5830b5b0c332fc64e48c6aeb8c25da421f2e5b8b
Binlong Li <herbert19lee@gmail.com>
2016-06-21 23:44:24 -0700

    Q074 not work.

--- leetCode/Q074SearchA2DMatrix.cpp
+++ leetCode/Q074SearchA2DMatrix.cpp
@@ -16,3 +16,40 @@
 
 
 // Given target = 3, return true.
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+        if(target < matrix[0][0]) return false;
+        if(target > matrix[m - 1][n - 1]) return false;
+        int row = m / 2;
+        // per row binary search
+        while(row > 0 && row < m) {
+            if(target == matrix[row][n - 1] || target == matrix[row - 1][n - 1]) return true;
+            if(target < matrix[row][n - 1] && target > matrix[row - 1][n - 1] ) break;
+            if(target < matrix[row][n - 1]) {
+                row = row / 2;
+            } else {
+                row = (row + 1 + m - 1) / 2;
+            }
+        }
+        int start = 0, end = n - 1;
+        int col = (start + end) / 2;
+        // per col binary search
+        if (target == matrix[row][0]) return true;
+        while(col >= 0 && col < n && start <= end) {
+            if(target == matrix[row][col]) return true;
+            if(target < matrix[row][col]){
+                end = col - 1;
+                col = (start + end) /2;
+            } else {
+                start = col + 1;
+                col = (start + end) / 2;
+            }
+        }
+        return false;
+    }
+};
