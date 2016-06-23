be7c557cb4082f81c2a4ac8c0f6f8876936eb27a
Binlong <bil@microsoft.com>
2016-06-22 23:05:58 -0700

    finish Q074

--- leetCode/Q074SearchA2DMatrix.cpp
+++ leetCode/Q074SearchA2DMatrix.cpp
@@ -19,37 +19,40 @@
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        if (m == 0) return false;
-        int n = matrix[0].size();
-        if(target < matrix[0][0]) return false;
-        if(target > matrix[m - 1][n - 1]) return false;
-        int row = m / 2;
-        // per row binary search
-        while(row > 0 && row < m) {
-            if(target == matrix[row][n - 1] || target == matrix[row - 1][n - 1]) return true;
-            if(target < matrix[row][n - 1] && target > matrix[row - 1][n - 1] ) break;
-            if(target < matrix[row][n - 1]) {
-                row = row / 2;
+    int binarySearchRow(const vector<vector<int>>& matrix, int target, int start, int end){
+        if(start <= end){
+            int mid = (start + end) / 2;
+            if (target >= matrix[mid][0] && target <= matrix[mid][n - 1]){
+                return mid;
+            }
+            if (target < matrix[mid][0]){
+                return binarySearchRow(matrix, target, start, mid - 1);
             } else {
-                row = (row + 1 + m - 1) / 2;
+                return binarySearchRow(matrix, target, mid + 1, end);
             }
         }
-        int start = 0, end = n - 1;
-        int col = (start + end) / 2;
-        // per col binary search
-        if (target == matrix[row][0]) return true;
-        while(col >= 0 && col < n && start <= end) {
-            if(target == matrix[row][col]) return true;
-            if(target < matrix[row][col]){
-                end = col - 1;
-                col = (start + end) /2;
-            } else {
-                start = col + 1;
-                col = (start + end) / 2;
-            }
+        return start == end ? start : end;
+    }
+    bool binarySearch(const vector<int>& matrix, int target, int start, int end){
+        if(start <= end){
+            int mid = (start + end) / 2;
+            if(target == matrix[mid]) return true;
+            if(target < matrix[mid]) return binarySearch(matrix, target, start, mid - 1);
+            else return binarySearch(matrix, target, mid + 1, end);
         }
         return false;
     }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        m = matrix.size();
+        if(m == 0) return false;
+        n = matrix[0].size();
+        if(target < matrix[0][0] || target > matrix[m - 1][n - 1]) {
+            return false;
+        }
+        int rowId = binarySearchRow(matrix, target, 0, m - 1);
+        return binarySearch(matrix[rowId], target, 0, n - 1);
+    }
+private:
+    int m;
+    int n;
 };
