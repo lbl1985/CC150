b47f7c1267960a328a35215b5cbc484cbb90d2bb
Binlong Li <herbert19lee@gmail.com>
2016-06-24 22:03:18 -0700

    finish Q215

--- leetCode/Q215_kthLargestEleentInAnArray.cpp
+++ leetCode/Q215_kthLargestEleentInAnArray.cpp
@@ -9,46 +9,19 @@
 
 class Solution {
 public:
-
-    int quicksort(vector<int>& nums, int k, int left, int right) {
-        int index = partition(nums, left, right);
-        if(index == n - k) {
-            return nums[index];
-        }
-        
-        if(index > n - k) {
-            if (left < index - 1) {
-                return quicksort(nums, k, left, index - 1);
-            } else {
-                return nums[left];
-            }
-        } else {
-            if(index < right) {
-                return quicksort(nums, k, index, right);
-            } else {
-                return nums[right];
-            }
-        }
-    }
-    
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[(left + right) / 2];
-        while(left <= right){
-            while(nums[left] < pivot) left++;
-            while(nums[right] > pivot) right--;
-            if(left <= right){
-                swap(nums[left], nums[right]);
-                left++;
-                right--;
-            }
-        }
-        return left;
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        n = nums.size();
-        return quicksort(nums, k, 0, n - 1);
+        int n = nums.size();
+        int tmp;
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(nums[j] > nums[j+1]){
+                    tmp = nums[j];
+                    nums[j] = nums[ j + 1];
+                    nums[j + 1] = tmp;
+                }
+            }
+        }
+        return nums[n - k];
+       
     }
-private:
-    int n;
 };
