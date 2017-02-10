95be5584ba35d842a7130c2e576618a6b327a863
Binlong Li <bil@microsoft.com>
2017-02-10 00:11:56 -0800

    prac grpah tree.

--- Chapter4_tree/GraphTest.cpp
+++ Chapter4_tree/GraphTest.cpp
@@ -56,11 +56,69 @@ void Graph::DFS(int x, int required)
 		return;
 	}
 	bool found = false;
-	
+	bool * visited = new bool [n];
+	for(int i = 0; i < n; i++) {
+		visited[i] = false;
+	}
+	visited[x - 1] = true;
+	stack<int> s;
+	s.push(x);
+	while(!s.empty()) {
+		int top = s.top();
+		printf("%d ", top);
+		if(top == required) {
+			found = true;
+			printConnected(x, required);
+			break;
+		}
+		s.pop();
+		for(int i = 1; i <= n; i++) {
+			if(isConnected(i, top) && visited[i - 1] == false) {
+				visited[i - 1] = true;
+				s.push(i);
+			}
+		}
+	}
+	if(found == false) {
+		printNotConnected(x, required);
+	}
+	delete [] visited;
 }
 
 void Graph::BFS(int x, int required)
 {
+	if(x == required) {
+		printConnected(x, required);
+		return;
+	}
+	bool found = false;
+	bool * visited = new bool [n];
+	for(int i = 0; i < n; i++) {
+		visited[i] = false;
+	}
+	visited[x - 1] = true;
+	queue<int> q;
+	q.push(x);
+	while(!q.empty()) {
+		int front = q.front();
+		printf("%d ", front);
+		if(front == required) {
+			found = true;
+			printConnected(x, required);
+			break;
+		}
+		q.pop();
+		for(int i = 1; i <= n; i++) {
+			if(isConnected(i, front) && visited[i - 1] == false) {
+				visited[i - 1] = true;
+				q.push(i);
+			}
+		}
+	}
+	if(found == false) {
+		printNotConnected(x, required);
+	}
+	delete [] visited;
 }
 
 int GraphTest(){
